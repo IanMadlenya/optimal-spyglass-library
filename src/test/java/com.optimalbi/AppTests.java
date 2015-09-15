f54f203971da6eab0ad7fc996fa4df933e0f7019
Timothy Gray <buzztimgray@gmail.com>
2015-09-15 16:26:57 +1200

    Added test folder, updated AWS library.

---
+++ src/test/java/com.optimalbi/AppTests.java
@@ -0,0 +1,11 @@
+package com.optimalbi;
+
+import org.junit.*;
+
+public class AppTests {
+    @Test
+    public void testJunit(){
+        //Test maven installed junit
+        Assert.assertTrue(true);
+    }
+}
